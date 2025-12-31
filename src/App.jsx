41491c22e4ef81dc3f2eea572730d32f2781eb21
Akanksha Goswami <>
2025-12-31 12:53:32 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -11,6 +11,7 @@ import AboutPage from "./pages/AboutPage";
 import TeamPage from "./pages/TeamPage";
 import TestimonialsPage from "./pages/TestimonialsPage";
 import FAQPage from "./pages/FAQPage";
+import BlogPage from "./pages/BlogPage";
 
 // Services
 import HomeLoan from "./pages/services/HomeLoan";
@@ -27,19 +28,17 @@ import Contact from "./components/Home/Contact";
 import ScrollToTop from "./components/ScrollToTop";
 import ScrollToTopButton from "./components/ScrollToTopButton";
 import WhatsAppButton from "./components/WhatsAppButton";
-import AdminRoutes from "./admin/auth/adminRoute";
 import CallButton from "./components/CallButton";
-import BlogPage from "./pages/BlogPage";
-
-// Admin
-// import AdminRoutes from "./routes/AdminRoutes";
+import AdminRoutes from "./admin/auth/adminRoute";
 
 /* ---------- USER LAYOUT ---------- */
-const UserLayout = ({ children }) => (
+const UserLayout = () => (
   <>
     <Header />
     <ScrollToTop />
-    <main className="min-h-screen">{children}</main>
+    <main className="min-h-screen">
+      <Outlet />
+    </main>
     <Footer />
     <CallButton />
     <WhatsAppButton />
@@ -49,44 +48,36 @@ const UserLayout = ({ children }) => (
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
+    <Routes>
 
-        {/* ---------- USER ROUTES ---------- */}
-        <Route
-          path="/*"
-          element={
-            <UserLayout>
-              <Routes>
-                <Route path="/" element={<HeroPage />} />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/team" element={<TeamPage />} />
-                <Route path="/testimonials" element={<TestimonialsPage />} />
-                <Route path="/faq" element={<FAQPage />} />
-                <Route path="/blogs" element={<BlogPage />} />
+      {/* ---------- USER ROUTES ---------- */}
+      <Route element={<UserLayout />}>
+        <Route path="/" element={<HeroPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/team" element={<TeamPage />} />
+        <Route path="/testimonials" element={<TestimonialsPage />} />
+        <Route path="/faq" element={<FAQPage />} />
+        <Route path="/blogs" element={<BlogPage />} />
 
-                {/* SERVICES */}
-                <Route path="/home-loan" element={<HomeLoan />} />
-                <Route path="/machinery-loan" element={<CarLoan />} />
-                <Route path="/personal-loan" element={<PersonalLoan />} />
-                <Route path="/business-loan" element={<BusinessLoan />} />
-                <Route path="/loan-against-property" element={<LoanAgainstProperty />} />
-                <Route path="/school-hospital-loan" element={<InstitutionalLoan />} />
-                <Route path="/industrial-commercial-loan" element={<CommercialIndustrialLoan />} />
-                <Route path="/corporate-funding" element={<CorporateFunding />} />
+        {/* SERVICES */}
+        <Route path="/home-loan" element={<HomeLoan />} />
+        <Route path="/machinery-loan" element={<CarLoan />} />
+        <Route path="/personal-loan" element={<PersonalLoan />} />
+        <Route path="/business-loan" element={<BusinessLoan />} />
+        <Route path="/loan-against-property" element={<LoanAgainstProperty />} />
+        <Route path="/school-hospital-loan" element={<InstitutionalLoan />} />
+        <Route path="/industrial-commercial-loan" element={<CommercialIndustrialLoan />} />
+        <Route path="/corporate-funding" element={<CorporateFunding />} />
+      </Route>
 
-                <Route path="*" element={<div className="py-20 text-center">Page Not Found</div>} />
-              </Routes>
-            </UserLayout>
-          }
-        />
+      {/* ---------- ADMIN ROUTES (NO HEADER/FOOTER) ---------- */}
+      <Route path="/admin/*" element={<AdminRoutes />} />
 
-        {/* ---------- ADMIN ROUTES (NO HEADER/FOOTER) ---------- */}
-        <Route path="/admin/*" element={<AdminRoutes />} />
+      {/* ---------- 404 ---------- */}
+      <Route path="*" element={<div className="py-20 text-center">Page Not Found</div>} />
 
-      </Routes>
-    </Router>
+    </Routes>
   );
 };
 
