7851e3e45ac6db9db3d4d95202ab8a3b57f236ce
Akanksha Goswami <107531046+goswami6@users.noreply.github.com>
2025-12-31 12:15:56 +0530

    Wrap App component with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import 'swiper/css';
@@ -8,6 +9,8 @@ import 'swiper/css/effect-fade';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
